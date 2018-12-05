efd3f8779240201c3fc2b5e52c9b4e795977bf0e
Uğur Oruç <uoruc5@hotmail.com>
2018-12-05 22:18:18 +0300

    folder added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { createAppContainer, createDrawerNavigator } from 'react-navigation';
 
-import Day1 from './pages/ui-1';
+import Day1 from './pages/ui-1/';
+import Day2 from './pages/ui-2/';
 
 const Navigator = createDrawerNavigator({
   Day1: {
     screen: Day1,
+  },
+  Day2: {
+    screen: Day2
   }
-});
+}, {
+    initialRouteName: 'Day2'
+  });
 
 export default createAppContainer(Navigator);
